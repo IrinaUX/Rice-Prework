7643188d5ad844ef9309e676bf4bcd59400ddf04
IrinaUX <irinahpda@gmail.com>
2020-02-10 22:43:19 -0600

    Update Udacity_JavaScript_Objects.js

--- Udacity_JavaScript_Objects.js
+++ Udacity_JavaScript_Objects.js
@@ -107,3 +107,25 @@ facebookProfile.postMessage("Hi there!");
 facebookProfile.postMessage("It's me, Irina");
 facebookProfile.deleteMessage(1);
 console.log(facebookProfile.messages);
+
+
+/*
+ * Programming Quiz: Donuts Revisited (7-6)
+ */
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+    ];
+
+
+//for (var elem = 0; elem < donuts.length; elem++) {
+//    console.log(donuts[elem]);
+//    }
+//}
+donuts.forEach(function(donut) {
+    console.log(donut.type + " donuts cost $" + donut.cost + " each");
+});
+// your code goes here
