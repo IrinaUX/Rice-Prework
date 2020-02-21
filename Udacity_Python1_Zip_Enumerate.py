de52f2c739b5ee1fd1d7c9f7eb17bb9b3bcfb59c
IrinaUX <irinahpda@gmail.com>
2020-02-20 23:05:26 -0600

    Create Udacity_Python1_Zip_Enumerate.py

---
+++ Udacity_Python1_Zip_Enumerate.py
@@ -0,0 +1,12 @@
+# ZIP and Enumerate
+
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+
+for label, x, y, z in zip(labels, x_coord, y_coord, z_coord):
+    point = "{}: {}, {}, {}".format(label, x, y, z)
+    print(point)
